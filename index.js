0e8a6116824c95eefa7f8c1787458a4b7d59eb73
Albega34 <160224869+Albega34@users.noreply.github.com>
2025-04-23 20:56:26 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Prabhakar hii')
+  res.send('Prabhakar hello')
 })
 
 app.get('/courses', function (req, res) {
